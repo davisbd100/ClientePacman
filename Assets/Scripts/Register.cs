b7b7ac1608470e9d7d5a4581f876161fb0fb1d1d
José Arturo <jarturogl05@gmail.com>
2019-11-13 00:32:21 -0600

    Agregar escena

---
+++ Assets/Scripts/Register.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Register : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TMP_InputField User_InputField;
+    public TMP_InputField Password_InputField;
+    public TMP_InputField Email_InputField;
+
+
+    public void register()
+    {
+
+    }
+}
