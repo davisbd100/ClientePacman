ae232160b3d824c3c805ce7d3e823f9fd2a8e956
José Arturo <jarturogl05@gmail.com>
2019-12-01 00:06:17 -0600

    Confirmación de cuenta

---
+++ Assets/Scripts/utilities/WrongCodeException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrongCodeException : Exception
+{
+    public WrongCodeException() :
+      base("Correo o usuario registrado en el sistema")
+    { }
+}
