3f33bf366a62ec0d89f643eb6d36a82632de4141
David <davisbd100@gmail.com>
2019-12-02 23:56:59 -0600

    Lobby en proceso
    Estructura de lobby, creacion de lobby players e interfaz basica

---
+++ Assets/Scripts/MultiPlayer/LobbyPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class LobbyPlayer : NetworkLobbyPlayer
+{
+    public GameObject parentPref;
+    public override void OnClientEnterLobby()
+    {
+        base.OnClientEnterLobby();
+        parentPref = GameObject.FindGameObjectWithTag("parentPref");
+        gameObject.transform.SetParent(parentPref.transform);
+    }
+}
---
+++ Assets/Scripts/MultiPlayer/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class MainMenu : MonoBehaviour
+{
+    public InputField matchNameInput;
+    public LobbyManager lobbyManager;
+
+    public void OnClickHostButton()
+    {
+        lobbyManager.StartMatchMaker();
+        lobbyManager.matchMaker.CreateMatch(matchNameInput.text, (uint)lobbyManager.maxPlayers, true, "", "", "", 0, 0, lobbyManager.OnMatchCreate);
+
+    }
+}
